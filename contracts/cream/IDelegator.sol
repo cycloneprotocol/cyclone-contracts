1c756369f5788526b483e7d60480bf2916947894
bf945 <73311932+bf945@users.noreply.github.com>
2021-05-08 07:17:13 -0700

    Create IDelegator.sol

---
+++ contracts/cream/IDelegator.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.16;
+
+interface IDelegator {
+    function expScale() external view returns (uint);
+    function balanceOf(address owner) external view returns (uint);
+    function balanceOfUnderlying(address owner) external returns (uint);
+    function getAccountSnapshot(address account) external view returns (uint, uint, uint, uint);
+    function borrowRatePerBlock() external view returns (uint);
+    function supplyRatePerBlock() external view returns (uint);
+    function exchangeRateCurrent() external returns (uint);
+    function exchangeRateStored() external view returns (uint);
+    function getCash() external view returns (uint);
+    function accrueInterest() external returns (uint);
+    function underlying() external view returns (address);
+    function mint(uint mintAmount) external returns (uint);
+    function redeem(uint redeemTokens) external returns (uint);
+    function redeemUnderlying(uint redeemAmount) external returns (uint);
+}
